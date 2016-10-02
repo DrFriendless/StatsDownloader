0e395b45d94f322f1c8b3fd6c2da8805b6cfcc52
DrFriendless <friendless.farrell@gmail.com>
2016-10-02 11:57:57 +1100

    added log URL to httpd

--- httpd/src/main/kotlin/com/drfriendless/statsdownloader/httpd/Main.kt
+++ httpd/src/main/kotlin/com/drfriendless/statsdownloader/httpd/Main.kt
@@ -3,11 +3,16 @@ package com.drfriendless.statsdownloader.httpd
 import org.slf4j.LoggerFactory
 import org.wasabifx.wasabi.app.AppConfiguration
 import org.wasabifx.wasabi.app.AppServer
+import org.wasabifx.wasabi.protocol.http.Response
+import org.wasabifx.wasabi.protocol.http.StatusCodes
 import org.wasabifx.wasabi.routing.RouteHandler
+import java.io.File
 
 /**
  * Created by john on 2/10/16.
  */
+class Thing: Object() {}
+
 fun AppServer.getLogError(path: kotlin.String, vararg handlers: RouteHandler.() -> kotlin.Unit): kotlin.Unit {
     val logger = org.slf4j.LoggerFactory.getLogger("handler")
     fun wrap(f: RouteHandler.() -> kotlin.Unit): RouteHandler.() -> kotlin.Unit {
@@ -25,16 +30,34 @@ fun AppServer.getLogError(path: kotlin.String, vararg handlers: RouteHandler.() 
 
 fun main(args: Array<String>) {
     val httpdConfig = AppConfiguration()
+    httpdConfig.port = 8084
     val server = AppServer(httpdConfig)
     val logger = LoggerFactory.getLogger("main")
     server.getLogError("/", {
 //        response.contentType = "text/html"
 //        response.send(renderPuzzle(puzzle))
     })
-    server.getLogError("/images/:icon", {
-//        val icon = request.routeParams["icon"]
-//        response.setFileResponseHeaders(serveFile("/icons/svg/$icon"), "image/svg+xml")
+    server.getLogError("/log", {
+        val f = File("/home/john/subjects/geek/downloader.log")
+        if (f.exists()) {
+            val url = f.toURI().toURL()
+            response.send(url.readText(), "text/plain")
+        } else {
+            response.returnFileContents("/html/noDownloadLog.html", "text/html")
+        }
     })
     logger.info("Starting Downloader httpd")
     server.start()
+}
+
+fun Response.returnFileContents(path: String, contentType: String) {
+    val u = Thing::class.java.getResource(path)
+    println("returnFileContents $path $u")
+    if (u != null) {
+        val text = u.readText()
+        this.contentLength = text.length.toLong()
+        send(text, contentType)
+    } else {
+        setStatus(StatusCodes.NotFound)
+    }
 }
